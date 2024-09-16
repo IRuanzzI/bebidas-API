730811188f8c5912d5afa690882162f01b5d6b97
Ruan Pablo <rp824194@gmail.com>
2024-09-16 16:52:38 -0300

    Update WebConfig.java

--- src/main/java/com/lana/vitor/servico_bebdidas/config/WebConfig.java
+++ src/main/java/com/lana/vitor/servico_bebdidas/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://bebidas-api.onrender.com", "http://localhost:8080") // URL do frontend
+                .allowedOrigins("http://localhost:3000", "https://bebidas-api.onrender.com", "http://localhost:8080", "https://front-blend.vercel.app") // URL do frontend
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
